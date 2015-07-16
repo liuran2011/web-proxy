bb16548d303fa885061c7a4ba8361d50ec1d1b37
LiuRan <liuran@fnic.cn>
2015-07-16 21:38:39 +0800

    check 9090 port state.

--- web_proxy.sh
+++ web_proxy.sh
@@ -9,11 +9,20 @@ do
     if [ "x$pid" == "x" ]; then
         echo "contrail-vrouter-agent not start. try later..." >>$log_file
         sleep 1
-    else
-        echo "contrail-vrouter-agent start ok. start web_proxy.py" >>$log_file
-        cd /home/web-proxy
-        ./deploy.sh
-        ./start.sh
-        break
+        continue
     fi
+   
+    state=$(netstat -lt| grep 9090)
+
+    if [ "x$state" == "x" ]; then
+        echo "contrail-vrouter-agent 9090 port is not listen..." >>$log_file
+        continue
+    fi
+
+    break
 done
+
+echo "contrail-vrouter-agent start ok. start web_proxy.py" >>$log_file
+cd /home/web-proxy
+./deploy.sh
+./start.sh
